68d911f200ca07a23a1249bbcbb25af65e34a050
Luis Fernando Nestares Valverde <68039881+luisfernando1901@users.noreply.github.com>
2021-11-04 14:49:00 -0500

    Se cambió al baseUrl de producción.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000',
-  //baseUrl: 'https://fonogram-c82df.uc.r.appspot.com'
+  //baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://fonogram-c82df.uc.r.appspot.com'
 };
